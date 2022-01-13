71cfe034e36be4b774f3a0aeac9f90d52ebb2510
Diego Goes <diego@vsktechnology.com.br>
2022-01-13 01:41:32 -0300

    Refatoração do todo o código.

--- js/script.js
+++ js/script.js
@@ -12,55 +12,73 @@ let expressao = [];
 let n1 = ``;
 let operador;
 let n2 = ``;
-let resultado;
+let resultado = ``;
 let limitadorAns = ``;
 let operadores = [`÷`, `×`, `-`, `+`];
+
+function exibirDisplay(msg, cor, concat) {
+    let pDisplay = document.getElementById(`pDisplay`);
+    pDisplay.style.color = `${cor}`;
+    if (concat == true) {
+        pDisplay.innerHTML += `${msg}`;
+    } else {
+        pDisplay.innerHTML = `${msg}`;
+    }
+}
+
 function salvarExpressao(char) {
+    /*
+    Tendo um expressao anterior, caso uma nova seja gerada, sendo ela inicialmente composta por um número, a expressão
+    deve ser zerada e o novo numero adiocionado a ela. Caso inicialmente seja composta por um operador matematico
+    ela deve manter a expressão com o resultado anterior e proseguir a a expressao.
+    */
+
     // Parte que salva a operação
     expressao += char;
-    console.log(`char: ${char} | expressao: ${expressao}`)
-    // Validar input, quando o primeiro char é um operador.
-    if (expressao.startsWith(`,`) || expressao.startsWith(`÷`) || expressao.startsWith(`×`)|| expressao.startsWith(`+`)) {
+
+    // Validar expressão quando o primeiro char não é um número.
+    if (expressao.startsWith(`,`) || expressao.startsWith(`÷`) || expressao.startsWith(`×`) || expressao.startsWith(`+`)) {
         expressao = [];
         console.log(`char: ${char} | expressao: ${expressao}`)
+        console.log(`Resultado-Anterior: ${resultado}`);
     } else {
-        // Parte de exibição
-        let pDisplay = document.getElementById(`pDisplay`);
-        pDisplay.style.color = `orange`;
         // Verificar calculo ANS, que seria o de multiplas expressões de uma vez.
         for (let i in operadores) {
             if (char == operadores[i]) {
-                // Indentifica todos os operadores da expressão matemática
-                limitadorAns += expressao.split(``).filter((op) => {
-                    console.log(`op: ${op} | operadores[i] | bollean: ${op == operadores[i]}`)
-                    if (op == operadores[i]) {
-                        return operadores[i]
-                    }
-                });
-                expressao = expressao.toString();
-                console.log(`limitadorAns: ${limitadorAns}`)
+
+                // Indentifica todos os operadores da expressão matemática e salva em limitadaorAns
+                for (let j in operadores) {
+                    limitadorAns += expressao.split(``).filter((charExpressao) => {
+                        if (charExpressao == operadores[j]) {
+                            return operadores[j];
+                        }
+                    });
+                }
+
+                // Le a quantidade de operadores no limitador, caso tenha mais de 1, ele efetua o calculo Ans.
                 if (limitadorAns.length >= 2) {
-                    limitadorAns = limitadorAns[1];
-                    console.log(`Hora de calcular o ANS`)
+                    // Limpar o limitador para a próxima expressao
+                    limitadorAns = ``
+
+                    // Efetuar a primeira expressão sem o 2º operador.
                     let operador2 = expressao.split(``).pop();
                     expressao = expressao.substring(0, expressao.length - 1);
                     console.log(`expressao: ${expressao}`)
+
+                    // Adicionar a expressão o resultado com o 2º operador.
                     calcular();
                     expressao = expressao.concat(operador2);
-                    return pDisplay.innerHTML += `${char}`;
                 }
             }
         }
-        pDisplay.innerHTML += `${char}`;
+        exibirDisplay(char, `orange`, true);
         console.log(`char: ${char} | expressao: ${expressao}`)
     }
 }
 function separarExpressao() {
-    // Possiveis operadores
-    // Achar qual é o operador da operação matemática.
-    // As vezes a operação podem começar com o sinal negativo, por isso
-    //se deve retirar esse sinal da operação.
+    // Acha o operador da expressão
     let expressaoSplit = expressao.split(``);
+    // caso o primeiro nº seja negativo, ele se torna positivo para achar o próximo operador.
     if (expressao.startsWith(`-`)) {
         expressaoSplit.shift();
     }
@@ -73,18 +91,15 @@ function separarExpressao() {
     });
 
     // Achar o n1 e n2
-    console.log(`expressao.indexOf(operador): ${expressao.indexOf(operador, 1)}`)
     for (let i in expressao) {
         if (i < expressao.indexOf(operador, 1)) {
-            console.log(`expressao[${i}]: ${expressao[i]}`);
             n1 += expressao[i];
-            console.log(`n1: ${n1} | length: ${n1.length}`);
         } else if (i > expressao.indexOf(operador, 1)) {
-            console.log(`expressao[${i}]: ${expressao[i]}`);
             n2 += expressao[i];
-            console.log(`n2: ${n2} | length: ${n2.length}`);
         }
     }
+
+    // Corrigir formato de decimais para efetuar as operações.
     if (n1.indexOf(`,`) != -1) {
         n1 = Number(n1.replace(`,`, `.`));
     } else {
@@ -105,24 +120,48 @@ function mudarSinal() {
         expressao = `-${expressao}`;
         console.log(`MUDANÇA DE SINAL POSITIVO | n1: ${n1} | expressao: ${expressao}`);
     }
-    let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.innerHTML = `${expressao}`;
+    expressao = expressao.replace(`.`, `,`);
+    exibirDisplay(expressao,`orange`, false);
 }
 function porcentagem() {
-    expressao = (Number(expressao) / 100).toString();
-    let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.innerHTML = `${expressao}`;
+    // Formata, faz o calculo de um numero divido por cem, e salva em 'numero'.
+    let numero = (Number(expressao.replace(`,`, `.`)) / 100).toString();
+
+    // Valida se o valor é valido (apenas 1 numero), para efetuar a operação
+    if (numero == 'NaN') {
+        // Exibe uma mensagem de erro no display durante 1 segundo.
+        exibirDisplay(`Erro!`,`white`, false);
+
+        // Volta ao valor original do display
+        setTimeout(() => {
+            exibirDisplay(expressao,`orange`,false);
+        }, 500)
+    } else {
+        numero = numero.toString().replace(`.`, `,`);
+        expressao = numero;
+        resultado = expressao;
+        exibirDisplay(resultado,`orange`,false);
+    }
+    console.log(`expressao: ${expressao}`);
+    console.log(`resultado: ${resultado}`);
+    console.log(`operador: ${operador}`)
 }
+
 function limpar() {
-    let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.innerHTML = ``;
-    expressao = ``;
+    exibirDisplay(``);
+    expressao = [];
+    operador = ``;
+    resultado = ``;
     console.clear();
+    console.log(`Resultado: ${resultado}`);
+    console.log(`Expressao: ${expressao}`);
+    console.log(`Operador: ${operador}`);
+    console.log(`-----------------------`);
 }
 function calcular() {
     // Parte que calcula
     separarExpressao();
-    console.log(`n1: ${n1} | operador: ${operador} | n2: ${n2}`)
+    console.log(`Operação: (n1: ${n1}) (operador: ${operador}) (n2: ${n2})`)
     switch (operador) {
         case `÷`:
             resultado = (n1 / n2);
@@ -141,18 +180,18 @@ function calcular() {
             console.log(`resultado = (n1 + n2) | ${resultado} = (${n1} + ${n2})`)
             break;
     }
-    resultado = resultado.toString()
+    console.log(`Expressao: ${expressao}`);
+
+    // Formatar saída de nº decimais
+    resultado = resultado.toString();
     expressao = resultado;
     if (resultado.indexOf(`.`) != -1) {
         resultado = resultado.replace(`.`, `,`);
     }
     // Parte de exibição
-    let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.style.color = `white`;
-    pDisplay.innerHTML = resultado;
-    // Zerar expressao
-    //expressao = resultado;
-    //limitadorAns = 0;
+    exibirDisplay(resultado,`white`,false);
+
+    // Zerar variaveis para a próxima expressao
     n1 = ``;
     operador = ``;
     n2 = ``;
