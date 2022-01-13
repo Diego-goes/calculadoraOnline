5fa070ba1eb6b667eb1ec4901dfd54ffe5cf7398
Diego Goes <diego@vsktechnology.com.br>
2022-01-12 22:22:16 -0300

    Ajuste na variavel operacao para expressao

--- js/script.js
+++ js/script.js
@@ -8,61 +8,63 @@
 
 // - - - - - Criar um 'ans' quando um 2º operador for pressionado - - - - - //
 
-let operacao = [];
+let expressao = [];
 let n1 = ``;
 let operador;
 let n2 = ``;
 let resultado;
 let limitadorAns = ``;
 let operadores = [`÷`, `×`, `-`, `+`];
-function salvarOperacao(char) {
+function salvarExpressao(char) {
     // Parte que salva a operação
-    operacao += char;
-    console.log(`char: ${char} | operacao: ${operacao}`)
-    if (operacao.startsWith(`,`) || operacao.startsWith(`÷`) || operacao.startsWith(`×`)) {
-        operacao = operacao.replace(char, "");
-        console.log(`char: ${char} | operacao: ${operacao}`)
+    expressao += char;
+    console.log(`char: ${char} | expressao: ${expressao}`)
+    // Validar input, quando o primeiro char é um operador.
+    if (expressao.startsWith(`,`) || expressao.startsWith(`÷`) || expressao.startsWith(`×`)|| expressao.startsWith(`+`)) {
+        expressao = [];
+        console.log(`char: ${char} | expressao: ${expressao}`)
     } else {
         // Parte de exibição
         let pDisplay = document.getElementById(`pDisplay`);
-        console.log(`ERA PARA TER TROCADO DE CORRRRRR!`)
         pDisplay.style.color = `orange`;
+        // Verificar calculo ANS, que seria o de multiplas expressões de uma vez.
         for (let i in operadores) {
             if (char == operadores[i]) {
-                limitadorAns += operacao.split(``).filter((op) => {
+                // Indentifica todos os operadores da expressão matemática
+                limitadorAns += expressao.split(``).filter((op) => {
                     console.log(`op: ${op} | operadores[i] | bollean: ${op == operadores[i]}`)
                     if (op == operadores[i]) {
                         return operadores[i]
                     }
                 });
-                operacao = operacao.toString();
+                expressao = expressao.toString();
                 console.log(`limitadorAns: ${limitadorAns}`)
                 if (limitadorAns.length >= 2) {
                     limitadorAns = limitadorAns[1];
                     console.log(`Hora de calcular o ANS`)
-                    let operador2 = operacao.split(``).pop();
-                    operacao = operacao.substring(0, operacao.length - 1);
-                    console.log(`operacao: ${operacao}`)
+                    let operador2 = expressao.split(``).pop();
+                    expressao = expressao.substring(0, expressao.length - 1);
+                    console.log(`expressao: ${expressao}`)
                     calcular();
-                    operacao = operacao.concat(operador2);
+                    expressao = expressao.concat(operador2);
                     return pDisplay.innerHTML += `${char}`;
                 }
             }
         }
         pDisplay.innerHTML += `${char}`;
-        console.log(`char: ${char} | operacao: ${operacao}`)
+        console.log(`char: ${char} | expressao: ${expressao}`)
     }
 }
-function separarOperacao() {
+function separarExpressao() {
     // Possiveis operadores
     // Achar qual é o operador da operação matemática.
     // As vezes a operação podem começar com o sinal negativo, por isso
     //se deve retirar esse sinal da operação.
-    let operacaoSplit = operacao.split(``);
-    if (operacao.startsWith(`-`)) {
-        operacaoSplit.shift();
+    let expressaoSplit = expressao.split(``);
+    if (expressao.startsWith(`-`)) {
+        expressaoSplit.shift();
     }
-    operador = operacaoSplit.find((char) => {
+    operador = expressaoSplit.find((char) => {
         for (let i in operadores) {
             if (char == operadores[i]) {
                 return operadores[i];
@@ -71,15 +73,15 @@ function separarOperacao() {
     });
 
     // Achar o n1 e n2
-    console.log(`operacao.indexOf(operador): ${operacao.indexOf(operador, 1)}`)
-    for (let i in operacao) {
-        if (i < operacao.indexOf(operador, 1)) {
-            console.log(`operacao[${i}]: ${operacao[i]}`);
-            n1 += operacao[i];
+    console.log(`expressao.indexOf(operador): ${expressao.indexOf(operador, 1)}`)
+    for (let i in expressao) {
+        if (i < expressao.indexOf(operador, 1)) {
+            console.log(`expressao[${i}]: ${expressao[i]}`);
+            n1 += expressao[i];
             console.log(`n1: ${n1} | length: ${n1.length}`);
-        } else if (i > operacao.indexOf(operador, 1)) {
-            console.log(`operacao[${i}]: ${operacao[i]}`);
-            n2 += operacao[i];
+        } else if (i > expressao.indexOf(operador, 1)) {
+            console.log(`expressao[${i}]: ${expressao[i]}`);
+            n2 += expressao[i];
             console.log(`n2: ${n2} | length: ${n2.length}`);
         }
     }
@@ -96,30 +98,30 @@ function separarOperacao() {
 }
 function mudarSinal() {
     //alert(`MUDAR DE SINAL | n1: ${n1}`)
-    if (operacao.startsWith(`-`)) {
-        operacao = operacao.replace(`-`, ``);
-        console.log(`MUDANÇA DE SINAL NEGATIVO | n1: ${n1} | operacao: ${operacao}`);
+    if (expressao.startsWith(`-`)) {
+        expressao = expressao.replace(`-`, ``);
+        console.log(`MUDANÇA DE SINAL NEGATIVO | n1: ${n1} | expressao: ${expressao}`);
     } else {
-        operacao = `-${operacao}`;
-        console.log(`MUDANÇA DE SINAL POSITIVO | n1: ${n1} | operacao: ${operacao}`);
+        expressao = `-${expressao}`;
+        console.log(`MUDANÇA DE SINAL POSITIVO | n1: ${n1} | expressao: ${expressao}`);
     }
     let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.innerHTML = `${operacao}`;
+    pDisplay.innerHTML = `${expressao}`;
 }
 function porcentagem() {
-    operacao = (Number(operacao) / 100).toString();
+    expressao = (Number(expressao) / 100).toString();
     let pDisplay = document.getElementById(`pDisplay`);
-    pDisplay.innerHTML = `${operacao}`;
+    pDisplay.innerHTML = `${expressao}`;
 }
 function limpar() {
     let pDisplay = document.getElementById(`pDisplay`);
     pDisplay.innerHTML = ``;
-    operacao = ``;
+    expressao = ``;
     console.clear();
 }
 function calcular() {
     // Parte que calcula
-    separarOperacao();
+    separarExpressao();
     console.log(`n1: ${n1} | operador: ${operador} | n2: ${n2}`)
     switch (operador) {
         case `÷`:
@@ -140,7 +142,7 @@ function calcular() {
             break;
     }
     resultado = resultado.toString()
-    operacao = resultado;
+    expressao = resultado;
     if (resultado.indexOf(`.`) != -1) {
         resultado = resultado.replace(`.`, `,`);
     }
@@ -148,8 +150,8 @@ function calcular() {
     let pDisplay = document.getElementById(`pDisplay`);
     pDisplay.style.color = `white`;
     pDisplay.innerHTML = resultado;
-    // Zerar operacao
-    //operacao = resultado;
+    // Zerar expressao
+    //expressao = resultado;
     //limitadorAns = 0;
     n1 = ``;
     operador = ``;
